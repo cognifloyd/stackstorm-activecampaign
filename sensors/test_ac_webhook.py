78e7e616b83262b1b625364fb6da22a3800caa8f
blag <blag@users.noreply.github.com>
2020-02-06 17:12:09 -0700

    Fixups for Python 3

--- sensors/test_ac_webhook.py
+++ sensors/test_ac_webhook.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import requests
 import json
 import yaml
@@ -24,4 +26,4 @@ data = "contact%5Bid%5D=903&contact%5Bemail%5D=ivan%40grozny.me" + \
        "&contact%5Bfields%5D%5Beula%5D=%7C%7CYes%2C+I+agree+to+the+terms+of+EULA%7C%7C" + \
        "&seriesid=22"
 response = requests.post(url, data=data, headers=headers)
-print json.dumps(response.json(), indent=4)
+print(json.dumps(response.json(), indent=4))
